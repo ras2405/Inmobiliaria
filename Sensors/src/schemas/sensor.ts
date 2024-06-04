14cd67a6f736375e6545a15092bef3f44e4ba3bd
ras2405 <rominar2000@gmail.com>
2024-06-04 20:14:01 -0300

    still ignores the zod lib validation

--- Sensors/src/schemas/sensor.ts
+++ Sensors/src/schemas/sensor.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 
 export const sensorSchema = z.object({
     id: z.string(),
-    description: z.string().max(200, 'Description must be less than 200 characters.'),
-    series: z.string().min(45, 'The series must be exactly 45 characters.').max(45, 'The series must be exactly 45 characters.'),
-    brand: z.string().max(50, 'Brand must be less than 50 characters.'),
-    address: z.string().max(1000, 'Address must be less than 1000 characters.'),
+    description: z.string().array().max(200, 'Description must be less than 200 characters.'),
+    series: z.string().array().length(45, 'The series must be exactly 45 characters.'),
+    brand: z.string().array().max(50, 'Brand must be less than 50 characters.'),
+    address: z.string().array().max(1000, 'Address must be less than 1000 characters.'),
     date: z.date(),
     type: z.string(),
     observableProperties: z.string()// z.array(z.string())
--- Sensors/src/services/sensorsService.ts
+++ Sensors/src/services/sensorsService.ts
@@ -3,6 +3,8 @@ import { SensorDto } from "../schemas/sensor";
 
 export const createSensor = async (sensorDto: SensorDto) => {
     if (!sensorDto) throw Error('Body vacío');
+    console.log("largo del series con el zod: ", sensorDto.series.length);
     let sensor = { ...sensorDto };
+    console.log("largo del series para el modelo de mongoose:", sensor.series.length);
     return await Sensor.create(sensor);
 };
