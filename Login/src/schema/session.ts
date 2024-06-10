e24b5b6d34ca319ced0de179a6f08f453b489986
ras2405 <rominar2000@gmail.com>
2024-06-10 09:16:47 -0300

    idem to last commit

--- Login/src/schema/session.ts
+++ Login/src/schema/session.ts
@@ -2,7 +2,8 @@ import { z } from 'zod';
 
 export const sessionSchema = z.object({
     mail: z.string().email(),
-    token: z.string()
+    token: z.string(),
+    role: z.enum(["Admin", "Operator", "Owner", "Tenant"])
 });
 
 export type SessionDto = z.infer<typeof sessionSchema>;
