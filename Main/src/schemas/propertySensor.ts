e87ce115dca12d967b83e2355b3e3eec98f6ef54
ras2405 <rominar2000@gmail.com>
2024-06-13 16:19:30 -0300

    added message if sensorId has more than 15 chars

--- Main/src/schemas/propertySensor.ts
+++ Main/src/schemas/propertySensor.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const PropertySensorSchema = z.object({
     propertyId: z.number(),
-    sensorId: z.string()
+    sensorId: z.string().max(15, 'sensorId must be less than 15 characters.'),
 });
