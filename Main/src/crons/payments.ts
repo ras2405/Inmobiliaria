b93c3973cd24ae814543d8a48cfa57a8203db31d
Daiana Aysa <da.aysa@gmail.com>
2024-06-17 09:22:08 -0300

    fix: delete console.log

--- Main/src/crons/payments.ts
+++ Main/src/crons/payments.ts
@@ -5,8 +5,6 @@ import { PaymentStatus } from '../constants/payments';
 
 const PAYMENT_TIMEOUT_MINUTES = parseInt(process.env.PAYMENT_TIMEOUT_MINUTES || '1440', 10); // 1440 minutos = 24 horas
 
-console.log('>>>> ', PAYMENT_TIMEOUT_MINUTES);
-
 const checkPendingPayments = async () => {
     const expirationTime = new Date(Date.now() - PAYMENT_TIMEOUT_MINUTES * 60 * 1000);
 
