09b38e476ddf6ce9d71a233b09860cbda7dbfcf8
GabrielGuerra404 <gabrielguerra404@gmail.com>
2024-06-18 00:33:37 -0300

    Added get booking as admin with filters, added utils file

--- Main/src/routes/bookingRoutes.ts
+++ Main/src/routes/bookingRoutes.ts
@@ -2,8 +2,13 @@ import { Router } from 'express';
 import * as bookingsController from '../controllers/bookingController';
 import { validate } from '../middlewares/validate';
 import { bookingSchema } from '../schemas/booking';
+import { validateParams } from '../middlewares/validateParams';
+import { getBookingsAsAdminOperator } from '../services/bookingsService';
+import { bookingFilterSchema } from '../schemas/bookingFilter';
+
 const router = Router();
 
 router.post('/',validate(bookingSchema), bookingsController.createBooking);
+router.get('/',validateParams(bookingFilterSchema), bookingsController.getBookings);
 
 export default router;
