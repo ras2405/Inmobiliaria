37d5fe28841b549860f3f5eb5d8dc45a553fd011
ras2405 <rominar2000@gmail.com>
2024-06-18 18:34:41 -0300

    added role verification in sensor creation

--- Login/src/routes/sessionRoutes.ts
+++ Login/src/routes/sessionRoutes.ts
@@ -1,11 +1,19 @@
-import { Router } from 'express';
+import { NextFunction, Router, Response } from 'express';
 import * as sessionController from '../controller/sessionController';
 import { validate } from '../middlewares/validate';
 import { sessionSchema } from '../schema/session';
+import { authenticateToken, CustomRequest } from '../../../Main/src/middlewares/loginConfig';
 
 const router = Router();
 
-router.post('/', validate(sessionSchema), sessionController.login);
+const authMiddleware = (role: string) => (req: CustomRequest, res: Response, next: NextFunction) =>
+    authenticateToken(req, res, next, role);
+
+router.post(
+    '/',
+    validate(sessionSchema),
+    authMiddleware('Admin'),
+    sessionController.login);
 router.get('/:token', sessionController.getSession);
 
 export default router;
