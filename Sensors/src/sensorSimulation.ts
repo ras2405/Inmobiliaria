b45c198969d73bcc6649d7be8734608c14babb5a
ras2405 <rominar2000@gmail.com>
2024-06-19 17:51:54 -0300

    sensor sim corrections

--- Sensors/src/sensorSimulation.ts
+++ Sensors/src/sensorSimulation.ts
@@ -1,70 +1,122 @@
-type SensorData = {
-    sensorId: string;
-    HumedadAmbiente: {
-        value: string;
-    };
-    Temperatura: {
-        Unidad: string;
-        value: string;
+import { SensorValueDto } from "./schemas/sensorValue";
+
+// type SensorData = {
+//     sensorId: string;
+//     HumedadAmbiente: {
+//         value: string;
+//     };
+//     Temperatura: {
+//         Unidad: string;
+//         value: string;
+//     };
+// };
+
+let intervalId: NodeJS.Timeout | null = null;
+let requestRatePerMinute: number = 0;
+const sensors: string[] = [];
+
+export const constructor = async (requestRatePerMinute: number, sensors: string[]) => {
+    requestRatePerMinute = requestRatePerMinute;
+    sensors = sensors;
+};
+
+export const getUpdatedValue = (value: string | number) => {
+    if (typeof value === 'number') {
+        return randomNumber(value);
+    }
+    return randomBool();
+};
+
+export const randomBool = () => (Math.random() > 0.5 ? "true" : "false");
+export const randomNumber = (max: number) => Math.random() * max;
+
+export const generateSensorData = async (sensorId: string, existingSensorData?: Partial<SensorValueDto>) => {
+
+    return {
+        id: sensorId,
+        ...(existingSensorData?.Humidity && {
+            Humidity: {
+                ...existingSensorData.Humidity,
+                value: getUpdatedValue(existingSensorData.Humidity.value)
+            }
+        }),
+        ...(existingSensorData?.Temperature && {
+            Temperature: {
+                ...existingSensorData.Temperature,
+                value: getUpdatedValue(existingSensorData.Temperature.value)
+            }
+        }),
+        ...(existingSensorData?.Electricity && {
+            Electricity: {
+                ...existingSensorData.Electricity,
+                value: getUpdatedValue(existingSensorData.Electricity.value)
+            }
+        }),
+        ...(existingSensorData?.DoorLock && {
+            DoorLock: {
+                ...existingSensorData.DoorLock,
+                value: getUpdatedValue(existingSensorData.DoorLock.value)
+            }
+        }),
+        ...(existingSensorData?.WindowLock && {
+            WindowLock: {
+                ...existingSensorData.WindowLock,
+                value: getUpdatedValue(existingSensorData.WindowLock.value)
+            }
+        }),
+        ...(existingSensorData?.Water && {
+            Water: {
+                ...existingSensorData.Water,
+                value: getUpdatedValue(existingSensorData.Water.value)
+            }
+        }),
+        ...(existingSensorData?.Gas && {
+            Gas: {
+                ...existingSensorData.Gas,
+                value: getUpdatedValue(existingSensorData.Gas.value)
+            }
+        }),
+        ...(existingSensorData?.Smoke && {
+            Smoke: {
+                ...existingSensorData.Smoke,
+                value: getUpdatedValue(existingSensorData.Smoke.value)
+            }
+        })
     };
 };
 
-class SensorSimulator {
-    private intervalId: NodeJS.Timeout | null = null;
-    private requestRatePerMinute: number;
-    private sensors: string[];
-
-    constructor(requestRatePerMinute: number, sensors: string[]) {
-        this.requestRatePerMinute = requestRatePerMinute;
-        this.sensors = sensors;
+export const start = async () => {
+    if (intervalId) {
+        console.log("Simulator is already running");
+        return;
     }
 
-    private generateSensorData(sensorId: string): SensorData {
-        return {
-            sensorId: sensorId,
-            HumedadAmbiente: {
-                value: (Math.random() * 100).toFixed(2)
-            },
-            Temperatura: {
-                Unidad: "Celsius",
-                value: (Math.random() * 40).toFixed(2)
-            }
-        };
+    const interval = (60 / requestRatePerMinute) * 1000;
+    intervalId = setInterval(() => {
+        sensors.forEach(sensorId => {
+            const data = generateSensorData(sensorId);
+            console.log(JSON.stringify(data));
+        });
+    }, interval);
+};
+
+export const stop = async () => {
+    if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
     }
+};
 
-    public start(): void {
-        if (this.intervalId) {
-            console.log("Simulator is already running");
-            return;
-        }
-
-        const interval = (60 / this.requestRatePerMinute) * 1000;
-        this.intervalId = setInterval(() => {
-            this.sensors.forEach(sensorId => {
-                const data = this.generateSensorData(sensorId);
-                console.log(JSON.stringify(data));
-            });
-        }, interval);
+export const setRequestRatePerMinute = async (rate: number) => {
+    requestRatePerMinute = rate;
+    if (intervalId) {
+        stop();
+        start();
     }
+};
 
-    public stop(): void {
-        if (this.intervalId) {
-            clearInterval(this.intervalId);
-            this.intervalId = null;
-        }
-    }
+export const setSensors = async (sensors: string[]) => {
+    sensors = sensors;
+};
 
-    public setRequestRatePerMinute(rate: number): void {
-        this.requestRatePerMinute = rate;
-        if (this.intervalId) {
-            this.stop();
-            this.start();
-        }
-    }
-
-    public setSensors(sensors: string[]): void {
-        this.sensors = sensors;
-    }
-}
-
-export default SensorSimulator;
+// export default SensorSimulator;
